6ddd5b52e20e16b453cc2abb603a69c65821d718
Andrei Diaconu <andrey_diaconu@yahoo.com>
2016-12-18 21:51:08 +0200

    First working version

--- storage_viewer.js
+++ storage_viewer.js
@@ -1,8 +1,47 @@
 //alert(window.localStorage.getItem('se:fkey'));
 chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
-  alert(JSON.stringify(msg));
-
-   if (msg.action == 'SendIt') {
-      alert(window.localStorage.getItem('se:fkey'));
-   }
-});
+            //alert(JSON.stringify(msg));
+            //foreach key in localStoragekeys create a table with first td name of key, second td value of key
+            //append the new html to the div below and inject it in the body
+            var keysToSearch = [];
+            var localStoredKeys = [];
+            var returnedKeys = {};
+            for (i = 0; i < localStorage.length; i++)   {
+                localStoredKeys.push(localStorage.key(i) + "=[" + localStorage.getItem(localStorage.key(i)) + "]");
+            }
+            $.each(msg.localStorageKeys, function(index, key) {
+                if (key != "") {
+                    keysToSearch.push(key);
+                }
+              });
+                $.each(msg.sessionStorageKeys, function(index, key) {
+                    if (key != "") {
+                        keysToSearch.push(key);
+                    }
+                });
+                $.each(keysToSearch, function(index, key) {
+                  returnedKeys[key] = localStorage.getItem(key);
+                  // var indexOfKey = jQuery.inArray(key, msg.localStorageKeys);
+                  // if(indexOfKey !== -1){
+                  //   var keyValue = returnedKeys[index]
+                  // }
+                    // if (jQuery.inArray(key, msg.localStorageKeys) !== -1) {
+                    //   returnedKeys[key] = key;
+                    // }
+                })
+                var html = '<div id="footerAndrei"' +
+                    ' style="position: fixed;' +
+                    'bottom: 0;width: 100%;' +
+                    'background: #0070FF;' +
+                    'line-height: 2;' +
+                    'text-align: center;' +
+                    'font-size: 30px;' +
+                    'font-family: sans-serif;' +
+                    'font-weight: bold;' +
+                    'text-shadow: 0 1px 0 #84BAFF;' +
+                    'box-shadow: 0 0 15px #00214B}">' + JSON.stringify(returnedKeys) + '</div>';
+                $($.parseHTML(html)).appendTo('body');
+                // if (msg.action == 'SendIt') {
+                //     alert(window.localStorage.getItem('se:fkey'));
+                // }
+            });
