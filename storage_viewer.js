dab17b6716a5f1c102f3466286ecf213df4a6a21
Andrei Diaconu <a.diaconu@levi9.com>
2017-01-12 13:24:08 +0200

    Add refresh button

--- storage_viewer.js
+++ storage_viewer.js
@@ -25,7 +25,7 @@ chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
 			htmlRows += htmlRow;
         }
     });
-
+	
     var html = '<div id="extensionOverlay"' +
         ' style="position: fixed;' +
         'top: 10px;' +
@@ -35,12 +35,27 @@ chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
         'font-family: sans-serif;' +
         'font-weight: bold;' +
         'border-radius:10px;' +
-        'background: rgba(54, 25, 25, .5);"><table><tr style="border-bottom:1pt solid white;"><th style="padding: 5px 20px 0 5px;">Key</th><th style="padding: 5px 20px 0 5px;">Value</th></tr>' + htmlRows + '</table></div>';
+        'background: rgba(54, 25, 25, .5);"><table><tr style="border-bottom:1pt solid white;">'+
+'<th style="padding: 5px 20px 0 5px;">Key</th><th style="padding: 5px 20px 0 5px;">Value<div style="float: right;cursor:pointer;color:white"id="Refresh">Refresh</div></th></tr>' + htmlRows + '</table></div>';
     chrome.storage.local.set({
         'extensionState': "open",
     });
     $($.parseHTML(html)).appendTo('body');
 });
+document.addEventListener( "click", function(event){
+	    var element = event.target;
+    if(element.id == "Refresh"){
+            chrome.storage.local.get(['keysToTrack', 'extensionState'], function(result) {
+        chrome.tabs.query({
+            active: true,
+            currentWindow: true
+        }, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, result, function(response) {});
+        });
+    });
+    }
+} );
+
 
 Object.byString = function(o, s) {
     s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
@@ -56,3 +71,4 @@ Object.byString = function(o, s) {
     }
     return o;
 }
+
