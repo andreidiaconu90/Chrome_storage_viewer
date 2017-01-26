b1aacc7d6b0bc754b2f50a659b92a1d1aae9f041
Andrei Diaconu <a.diaconu@levi9.com>
2017-01-26 16:51:57 +0200

    Rework save message

--- options.js
+++ options.js
@@ -18,10 +18,9 @@ function save_options() {
         'keysToTrack': keysToTrack,
     }, function() {
         // Update status to let user know options were saved.
-        var status = document.getElementById('status');
-        status.textContent = 'Options saved';
+        $("#saveMessage").show();
         setTimeout(function() {
-            status.textContent = '';
+          $("#saveMessage").hide();
         }, 1000);
     });
 }
