3b8d1098e1d263e4a614eeedc409ed886c3fd2bc
Andrei Diaconu <adiaconu@travix.com>
2017-11-01 11:00:33 +0200

    Extract inline css in .css file

--- storage_viewer.js
+++ storage_viewer.js
@@ -1,21 +1,5 @@
-const EXTENSION_OVERLAY_HTML = '<div id="extensionOverlay"' +
-    'style="position: fixed !important;' +
-    'top: 10px !important;' +
-    'right:5px !important;' +
-    'color:white !important;' +
-    'font-size: 12px !important;' +
-    'font-family: sans-serif !important;' +
-    'font-weight: bold !important;' +
-    'border-radius:10px !important;' +
-    'font-family:Helvetica !important;' +
-    'background: rgba(54, 25, 25, .5) !important;z-index: 888888888888  !important;"></div>';
-const NO_OPTIONS_CONFIGURED = '<p style="padding: 8px 5px 0 5px;!important">No options defined.<br/>Right click on the extension icon and click Options</p>'
-const OVERLAY_TABLE_STYLE = "margin-top:7px !important;margin-bottom:7px !important";
-const OVERLAY_TABLE_ROW_STYLE = "border-bottom:1pt solid white !important";
-const OVERLAY_TABLE_HEADER_STYLE = "padding:3px 10px 0 10px !important";
-const OVERLAY_REFRESH_BUTTON_STYLE = "width:20px !important;height:20px !important;display:block !important;float: right !important;cursor:pointer !important;";
-const OVERLAY_COPY_BUTTON_STYLE = "width:15px;height:15px;display:block;cursor:pointer;";
-const VALUE_INPUT_STYLE = "background:none;border:none;width: 100%;color:white !important;font-family:Helvetica !important;max-width:300px;";
+const EXTENSION_OVERLAY_HTML = '<div id="extensionOverlay"></div>';
+const NO_OPTIONS_CONFIGURED = '<p id="noOptionsConfigured">No options defined.<br/>Right click on the extension icon and click Options</p>'
 var selectedType = {
     None: "0",
     LocalStorage: "1",
@@ -25,38 +9,6 @@ var selectedType = {
 };
 
 
-chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
-    displayOverlay(msg, sender, sendResponse);
-});
-
-document.addEventListener("keypress", function(event) {
-    var key = event.which || event.keyCode;
-    var element = event.target;
-    if (element.className === "inputValue" && key === 13) {
-        saveUpdatedValue(element);
-    }
-});
-
-document.addEventListener("click", function(event) {
-    var element = event.target;
-    if (element.id == "Refresh") {
-        chrome.storage.local.get(['keysToTrack', 'extensionState'], function(result) {
-            var isRefresh = true;
-            var isSuccess = displayOverlay(result, undefined, 'isRefresh');
-
-            if (isSuccess) {
-                showMessage("refresh");
-            }
-        });
-    } else if (element.className === "copyToClipboard") {
-        copyToClipboard(element);
-        showMessage("copy");
-    } else if (element.className === "removeKey") {
-        removeKey(element);
-    } else if (element.className === "editValue") {
-        editValue(element);
-    }
-});
 
 function displayOverlay(msg, sender, sendResponse) {
     var extensionOverlay = EXTENSION_OVERLAY_HTML;
@@ -81,12 +33,12 @@ function displayOverlay(msg, sender, sendResponse) {
         $($.parseHTML(noOptionsConfigured)).appendTo('#extensionOverlay');
     } else {
         var refreshButtonUrl = chrome.extension.getURL('Refresh-20.png');
-        var table = '<table style="' + OVERLAY_TABLE_STYLE + '"><tr style="' + OVERLAY_TABLE_ROW_STYLE + '">' +
+        var table = '<table id="overlayTable"><tr id="overlayTableRow">' +
             '<th></th>' +
-            '<th style="' + OVERLAY_TABLE_HEADER_STYLE + '">Key</th>' +
-            '<th style="' + OVERLAY_TABLE_HEADER_STYLE + '">Value</th>' +
+            '<th class="overlayTableHeader">Key</th>' +
+            '<th class="overlayTableHeader">Value</th>' +
             '<th></th>' +
-            '<th style="padding-right:8px !important"><div style="' + OVERLAY_REFRESH_BUTTON_STYLE + ';background:url(' + refreshButtonUrl + ');"id="Refresh"></div></th>' +
+            '<th style="padding-right:8px !important"><div class="overlayRefreshButton" style="background:url(' + refreshButtonUrl + ');"id="Refresh"></div></th>' +
             '</tr>' + htmlRows +
             '</table><i id="refreshMessage" style="display=none;"></i>' +
             '<i id="copyMessage" style="display=none;"></i>';
@@ -252,8 +204,8 @@ function generateHtml(keyHtml, valueHtml, showButtons, keyLocation, index) {
 
     var html = "<tr>" +
         "<td>" + deleteButtonDiv + "</td>" +
-        "<td style='" + OVERLAY_TABLE_HEADER_STYLE + "'>" + keyHtml + "</td><td id='storage-viewer-value-html-" + index + "' class='valueCell'style='padding:3px 5px 0 10px;max-width:300px;'>" +
-        "<input id='inputValue-" + index + "' type='text'class='inputValue' style='" + VALUE_INPUT_STYLE + "'; value='" + valueHtml + "'readonly/>" +
+        "<td class='overlayTableHeader'>" + keyHtml + "</td><td id='storage-viewer-value-html-" + index + "' class='valueCell'style='padding:3px 5px 0 10px;max-width:300px;'>" +
+        "<input id='inputValue-" + index + "' type='text'class='inputValue' value='" + valueHtml + "'readonly/>" +
         "</td>" +
         "<td class='table-cell'>" +
         editButtonDiv +
@@ -273,7 +225,7 @@ function saveOjectsAndArraysToLocalStorage(valueHtml, index){
     }
 }
 
-function isObject(value){
+function isObject(value) {
     return value && value.constructor === Object;
 }
 
@@ -301,7 +253,7 @@ function copyToClipboard(e) {
 
 function editValue(e) {
     var editedInput = $(e).parent().parent().find('input[readonly]');
-    $(editedInput).removeAttr('style').attr("readonly", false).css('color', 'black');
+    $(editedInput).attr("class","saveEditedValue").attr("readonly",false).css("color","black");
     $(editedInput).after('<p class="storage_viewer_tooltip" style="font-size:10px">Press Enter to save changes');
     $(editedInput).focus().select();
 }
@@ -333,7 +285,7 @@ function saveUpdatedValue(e) {
             }
         }
     }
-    $(editedInput).attr('style', VALUE_INPUT_STYLE);
+    $(editedInput).attr('class', "inputValue");
     $(editedInput).parent().find('.storage_viewer_tooltip').remove();
     $('#Refresh').click();
 }
@@ -342,14 +294,54 @@ function injectCss(url) {
     var path = chrome.extension.getURL(url);
 
     $('head').append($('<link>')
-        .attr("rel", "stylesheet")
-        .attr("type", "text/css")
-        .attr("href", path));
+             .attr("rel", "stylesheet")
+             .attr("type", "text/css")
+             .attr("href", path));
 }
 
 function getButtonDiv(buttonClass, buttonUrl) {
-    return "<div class='" + buttonClass + "' style='" + OVERLAY_COPY_BUTTON_STYLE + "background:url(" + buttonUrl + ")'></div>";
+    return "<div class='" + buttonClass + " overlayCopyButton' style='background:url(" + buttonUrl + ")'></div>";
 }
+
+
+//-----------------------EVENT LISTENERS--------------------------------------------
+
+chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
+    displayOverlay(msg, sender, sendResponse);
+});
+
+document.addEventListener("keypress", function(event) {
+    var key = event.which || event.keyCode;
+    var element = event.target;
+    if ($(element).hasClass("saveEditedValue") && key === 13) {
+       $(element).attr("class","inputValue");
+        saveUpdatedValue(element);
+    }
+});
+
+document.addEventListener("click", function(event) {
+    var element = event.target;
+    if (element.id == "Refresh") {
+        chrome.storage.local.get(['keysToTrack', 'extensionState'], function(result) {
+            var isRefresh = true;
+            var isSuccess = displayOverlay(result, undefined, 'isRefresh');
+
+            if (isSuccess) {
+                showMessage("refresh");
+            }
+        });
+    } else if ($(element).hasClass("copyToClipboard")) {
+        copyToClipboard(element);
+        showMessage("copy");
+    } else if ($(element).hasClass("removeKey")) {
+        removeKey(element);
+    } else if ($(element).hasClass("editValue")) {
+        editValue(element);
+    }
+});
+
+//-----------------------EVENT LISTENERS--------------------------------------------
+
 
 //do not remove as it caould be useful in the future
 // function refreshSavedOptionsAndRefreshOverlay(keyToRemove, keyLocation) {
